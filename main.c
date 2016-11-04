055564a04a8dcecee51f9d658b3bd22adb98e8a6
Theogorath <theo.drezenugurlu@viacesi.fr>
2016-11-04 09:21:09 +0100

    Commit to feature-subtraction

--- main.c
+++ main.c
@@ -5,9 +5,13 @@ int addition(int a, int b)
 {
     return a+b;
 }
-
+int substraction(int a, int b)
+{
+    return a-b;
+}
 int main()
 {
     printf("L’addition de a et de b vaut %d\n", addition(1,4));
+    printf("La soustraction de a et de b vaut %d\n", substraction(1,4));
     return 0;
 }
